81f24d27fc6df8ca155974093d68d3cac1301af0
Nithish2312 <pnithish.kumar2018@vitstudent.ac.in>
2021-07-17 21:15:23 +0530

    Updates

---
+++ precache-manifest.fd10f292a79db74d18ffb28d5b0f8e2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c83dff761e9bc5a16fcfca512780978",
+    "url": "/DYTE-FrontEndTask/index.html"
+  },
+  {
+    "revision": "d977db8d9ef9ae7e83bf",
+    "url": "/DYTE-FrontEndTask/static/css/2.6296f2c2.chunk.css"
+  },
+  {
+    "revision": "56e30cc226d0fedcd327",
+    "url": "/DYTE-FrontEndTask/static/css/main.d0c27ea9.chunk.css"
+  },
+  {
+    "revision": "d977db8d9ef9ae7e83bf",
+    "url": "/DYTE-FrontEndTask/static/js/2.1151a8f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DYTE-FrontEndTask/static/js/2.1151a8f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e30cc226d0fedcd327",
+    "url": "/DYTE-FrontEndTask/static/js/main.ecf3cc2d.chunk.js"
+  },
+  {
+    "revision": "90499a051c03feb336be",
+    "url": "/DYTE-FrontEndTask/static/js/runtime-main.8d3ec095.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/DYTE-FrontEndTask/precache-manifest.fd10f292a79db74d18ffb28d5b0f8e2a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DYTE-FrontEndTask/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
